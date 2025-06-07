c8ab0e3da430b5b7650325572b5b134e3d6bd214
9IZUKI9 <ggph640@gmail.com>
2025-06-07 17:01:52 +0700

    Initial Commit

---
+++ src/components/StoreProduct.tsx
@@ -0,0 +1,33 @@
+import '../Styles/StoreProduct.css'
+import { useState } from 'react';
+
+function StoreProduct({bg, column, row, width = '480px', height = '200px', name, fontSize = '30px'}) {
+
+    const [count, setCount] = useState(0)
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+
+    return (
+        <a
+        className='product-box' 
+        href='#' 
+        style={{
+            backgroundImage: `url(${bg})`,
+            gridArea: `${row} / ${column}`,
+            height,
+            width,
+        }}>
+            <h1 className='product-name' style={{fontSize}}>
+            {name}
+            </h1>
+
+            <div className='product-buy'>
+                <button onClick={decrement}>-</button>
+                <div>{count}</div>
+                <button onClick={increment}>+</button>
+            </div>
+        </a>
+    )
+}
+
+export default StoreProduct
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Store from './components/Store.tsx';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Store />
+  </React.StrictMode>
+);
