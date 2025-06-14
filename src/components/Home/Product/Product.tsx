19938fb152a7e7a271696171656e37a8547d1115
9IZUKI9 <ggph640@gmail.com>
2025-06-14 18:48:52 +0700

    Отступ, блять, добавил

--- src/components/Home/Product/Product.tsx
+++ src/components/Home/Product/Product.tsx
@@ -1,24 +1,24 @@
 import styles from './Product.module.css'
 import { useState } from 'react'
 
-function Product({bg, column, row, width = '480px', height = '200px', name, fontSize = '30px'}) {
+function Product({ bg, column, row, width = '480px', height = '200px', name, fontSize = '30px' }) {
     const [count, setCount] = useState(0)
     const increment = () => setCount(count + 1);
     const decrement = () => setCount(count - 1);
 
     return (
         <a
-        className={styles.product} 
-        href='#' 
-        style={{
-            backgroundImage: `url(${bg})`,
-            gridArea: `${row} / ${column}`,
-            height,
-            width,
-        }}>
-            
-            <h1 className={styles.product__name} style={{fontSize}}>
-            {name}
+            className={styles.product}
+            href='#'
+            style={{
+                backgroundImage: `url(${bg})`,
+                gridArea: `${row} / ${column}`,
+                height,
+                width,
+            }}>
+
+            <h1 className={styles.product__name} style={{ fontSize }}>
+                {name}
             </h1>
 
             <div className={styles.product__buy}>
